33a5eaa7f1633152a04ee211f961cb0ea4377c38
Daniel <d_abdurakhmanov@kbtu.kz>
2026-04-01 01:06:13 +0500

    lab 9

---
+++ lab9/shop-back/api/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'price', 'count',
+        'is_active', 'category'
+    )
+    list_filter = ('category', 'is_active')
+    list_editable = ('price', 'count', 'is_active')
+    search_fields = ('name', 'description')
---
+++ lab9/shop-back/api/views.py
@@ -0,0 +1,21 @@
+from .serializers import CategorySerializer, ProductSerializer
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Product, Category
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer 
+
+    @action(detail=True, methods=['get'], url_path='products')
+    def products(self, request, pk=None):
+        category = self.get_object()
+        qs = category.products.all()
+        serializer = ProductSerializer(qs, many=True)
+        return Response(serializer.data)
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
